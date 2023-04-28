680803a672e6a40a700bc6d6330fb92097e3aa52
David W <40841733+TheDWu@users.noreply.github.com>
2023-04-28 10:11:12 -0500

    Add files via upload
    implementation of pricing module

--- pricing_module.php
+++ pricing_module.php
@@ -1,20 +1,53 @@
 <?php
-class pricingModule {
+//require_once('db_conn.php');
 
-  private $nGallons; // number of gallons requested
-  private $margin; // margin value
-  private $currentPrice = 1.50; //  current price . Assignment says can hard code to $1.50
-  private $suggestedPrice; // suggested price per gallon = currentPrice + margin 
-  private $rateHistoryFactor; // bool value that says if the client is an existing customer
-  private $totalPrice; // total price
+//namespace Price;
 
+class pricing_module {
+    private $gallonsRequestedFactor = 0.03; // number of gallons requested
+    private $margin; // margin value
+    private $currentPrice = 1.50; //  current price . Assignment says can hard code to $1.50
+    private $suggestedPrice = 0; // suggested price per gallon = currentPrice + margin 
+    private $rateHistoryFactor = 0; //default is 0%, client gets 1% discount if they have requested fuel before
+    private $totalPrice; //total price
+    private $companyProfitMargin = 0.10; //company profit margin is always 10%
+    private $locationFactor = 0.04; //default location factor to 4%, changes to 2% if client is in Texas
+    //private $conn = mysqli_connect("localhost", "root", "", "fuel_database");
 
-  /* Functions below */
+    /* Functions below */
+    public function marginCalculator($state, $accountID, $gallonsRequested) { /*Calculate and set private variable margin */
+        //require_once('db_conn.php');
+        if ($state == "TX") {
+            $this->locationFactor = 0.02;
+        } 
 
-  function suggestedPrice (){ 
-    $this->suggestedPrice = $this->currentPrice + $this->margin;  // sets the value of suggested price
+        $conn = mysqli_connect("localhost", "root", "", "fuel_database");
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        $sql = "SELECT * FROM FuelQuote WHERE accountID='$accountID'";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) >= 1) {
+            $this->rateHistoryFactor = 0.01;
+        }
+        mysqli_close($conn);
+
+        if ($gallonsRequested > 1000) {
+            $this->gallonsRequestedFactor = 0.02;
+        }
+        
+        $this->margin = $this->currentPrice * ($this->locationFactor - $this->rateHistoryFactor + $this->gallonsRequestedFactor + $this->companyProfitMargin);
     }
-  function marginCalculator(/* Put needed arguments*/ ){ /*Calculate and set private variable margin */}
-}
 
+    public function getSuggestedPrice () { 
+        $this->suggestedPrice = $this->currentPrice + $this->margin;  // sets the value of suggested price per gallon
+        return $this->suggestedPrice;
+    }
+
+    public function getTotalPrice ($gallonsRequested) {
+        $this->totalPrice = $gallonsRequested * $this->suggestedPrice;
+        return $this->totalPrice; 
+    }
+}
 ?>
